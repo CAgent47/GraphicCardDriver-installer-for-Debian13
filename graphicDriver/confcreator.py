f7734c7d87000eb162e4d64673e4cb9841748529
CAgent_47 <linux@localhost.localdomain>
2026-07-03 18:48:14 +0330

    created python file for check json exists

---
+++ graphicDriver/confcreator.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+configuration = {
+    "packages": [
+        "build-essential",
+        "dkms",
+        "nvidia-detect"
+    ],
+    "next_packages": [
+        "nvidia-driver",
+        "nvidia-kernel-dkms"
+    ]
+}
+
+if os.path.exists('config.json'):
+    print("[ PYTHON NOTE ]: configuration file exists")
+else:
+    with open('config.json', 'w') as packages:
+        json.dump(packages, configuration)
+        print("[ PYTHON NOTE ]: configuration file created Runing Shell Code!")
