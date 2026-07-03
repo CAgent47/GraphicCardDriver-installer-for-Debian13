033195833244b036050eed57cdc6b5c8a04bb393
CAgent_47 <linux@localhost.localdomain>
2026-07-03 16:08:16 +0330

    Optimized Script and fixed bugs

--- graphicDriver/installer.sh
+++ graphicDriver/installer.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+function installer() {
+    if ! command -v "$@" &> /dev/null; then
+        echo '[ + ] installing '"$@"
+        sudo apt install -y "$@"
+    else
+        echo '[ - ] '"$@"' installed in your PC'
+    fi
+}
+sudo apt install -y linux-headers-$(uname -r)
 sudo sed -i 's/main/main non-free contrib/g' /etc/apt/sources.list
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install -y linux-headers-$(uname -r) build-essential dkms nvidia-detect
+pkg_basic=($(jq -r '.packages[]' config.json))
+pkg_next=($(jq -r '.next_Packages[]' config.json))
+
+for basic in "${pkg_basic[@]}"; do
+    installer $basic
+done
+
 nvidia-detect
-sudo apt install -y nvidia-driver nvidia-kernel-dkms
+
+for afterkg in "${pkg_next[@]}"; do
+    installer $afterkg
+done
+
 echo "restart your system Please"
 echo "Created By CAgent_47"
